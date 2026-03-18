3fabd573714a341735a8c574e7562502c2249b88
Majken <majros01@easv365.dk>
2026-03-18 10:45:49 +0100

    add Gauge set up

--- src/main/java/com/example/vindtved_projekt/Controller.java
+++ src/main/java/com/example/vindtved_projekt/Controller.java
@@ -1,16 +1,44 @@
 package com.example.vindtved_projekt;
 
+import eu.hansolo.medusa.Gauge;
+import eu.hansolo.medusa.GaugeBuilder;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.paint.Color;
 import javafx.util.Duration;
 
 public class Controller
 {
+    // Gauge til hver enkelt mølle
+    Gauge m1Gauge;
+    Gauge m2Gauge;
+    Gauge m3Gauge;
+    Gauge m4Gauge;
+    Gauge m5Gauge;
+    Gauge m6Gauge;
+
+    // Vind hastinghed m/s
+    Gauge vindmsGauge;
+
+    // Samlet effekt af alle møller
+    Gauge effektSamletGauge;
+
+    // Måneds Oversigt
+    CategoryAxis xAxisM = new CategoryAxis();
+    NumberAxis yAxisM = new NumberAxis();
+    LineChart<String, Number> lineChartMonth = new LineChart<>(xAxisM, yAxisM);
+    XYChart.Series<String, Number> seriesM = new XYChart.Series<>();
+
     private Timeline venteTimer;
 
     public void initialize()
     {
-
+        setUpGauges();
+        setUpLineChart();
     }
 
     // Metode der henter vindmølle data hvert 10. minut og gemmer det i vores database
@@ -24,5 +52,98 @@ public class Controller
         );
         venteTimer.setCycleCount(Timeline.INDEFINITE);
         venteTimer.play();
+    }
+
+    public void setUpGauges() {
+        m1Gauge = GaugeBuilder.create()
+                .skinType(Gauge.SkinType.FLAT)
+                .title("M1 Effect")
+                .unit("kW")
+                .maxValue(5000)
+                .animated(true)
+                .foregroundBaseColor(Color.rgb(0, 180, 0))
+                .barColor(Color.rgb(0, 180, 0))
+                .build();
+
+        m2Gauge = GaugeBuilder.create()
+                .skinType(Gauge.SkinType.FLAT)
+                .title("M2 Effect")
+                .unit("kW")
+                .maxValue(5000)
+                .animated(true)
+                .foregroundBaseColor(Color.rgb(0, 180, 0))
+                .barColor(Color.rgb(0, 180, 0))
+                .build();
+
+        m3Gauge = GaugeBuilder.create()
+                .skinType(Gauge.SkinType.FLAT)
+                .title("M3 Effect")
+                .unit("kW")
+                .maxValue(5000)
+                .animated(true)
+                .foregroundBaseColor(Color.rgb(0, 180, 0))
+                .barColor(Color.rgb(0, 180, 0))
+                .build();
+
+        m4Gauge = GaugeBuilder.create()
+                .skinType(Gauge.SkinType.FLAT)
+                .title("M4 Effect")
+                .unit("kW")
+                .maxValue(5000)
+                .animated(true)
+                .foregroundBaseColor(Color.rgb(0, 180, 0))
+                .barColor(Color.rgb(0, 180, 0))
+                .build();
+
+        m5Gauge = GaugeBuilder.create()
+                .skinType(Gauge.SkinType.FLAT)
+                .title("M5 Effect")
+                .unit("kW")
+                .maxValue(5000)
+                .animated(true)
+                .foregroundBaseColor(Color.rgb(0, 180, 0))
+                .barColor(Color.rgb(0, 180, 0))
+                .build();
+
+        m6Gauge = GaugeBuilder.create()
+                .skinType(Gauge.SkinType.FLAT)
+                .title("M6 Effect")
+                .unit("kW")
+                .maxValue(5000)
+                .animated(true)
+                .foregroundBaseColor(Color.rgb(0, 180, 0))
+                .barColor(Color.rgb(0, 180, 0))
+                .build();
+
+        vindmsGauge = GaugeBuilder.create()
+                .skinType(Gauge.SkinType.SIMPLE_DIGITAL)
+                .foregroundBaseColor(Color.rgb(0, 120, 255))
+                .barColor(Color.rgb(0, 120, 255))
+                .unit("m/s")
+                .maxValue(30)
+                .animated(true)
+                .build();
+
+        effektSamletGauge = GaugeBuilder.create()
+                .skinType(Gauge.SkinType.MODERN)
+                .title("Samlet Effekt")
+                .unit("kW")
+                .minorTickSpace(100)
+                .minValue(-5000)
+                .maxValue(5000)
+                .value(0)
+                .animated(true)
+                .build();
+
+        // Alle skal sættet på fxml
+    }
+
+    private void setUpLineChart(){
+        lineChartMonth.setAnimated(false);
+        lineChartMonth.setTitle("Måneds Oversigt over samlet effekt");
+        yAxisM.setLabel("Samlet effekt");
+        xAxisM.setLabel("Dato");
+        seriesM.setName("Samlet effekt");
+        // Skal sættet på fxml
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,6 +8,7 @@ module com.example.vindtved_projekt {
     requires java.naming;
     requires transitive com.microsoft.sqlserver.jdbc;
     requires java.sql;
+    requires eu.hansolo.medusa;
 
 
     opens com.example.vindtved_projekt to javafx.fxml;
